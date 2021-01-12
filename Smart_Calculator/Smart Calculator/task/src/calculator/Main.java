1aee90237342a430b4d671927893a9224e5cafee
bastihz <bastihz.dev@posteo.de>
2021-01-12 13:41:10 +0100

    Stage 1, 2, and 3 done

--- Smart_Calculator/Smart Calculator/task/src/calculator/Main.java
+++ Smart_Calculator/Smart Calculator/task/src/calculator/Main.java
@@ -6,6 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
+        while (true) {
+            String[] line = scanner.nextLine().trim().split("\\s+");
+            String first = line[0];
+            switch (first) {
+                case "/exit":
+                    System.out.println("Bye!");
+                    return;
+                case "/help":
+                    System.out.println("The program calculates the sum of numbers.");
+                    continue;
+                case "":
+                    continue;
+            }
+            int result = 0;
+            for (String s : line) {
+                result += Integer.parseInt(s);
+            }
+            System.out.println(result);
+        }
     }
 }
