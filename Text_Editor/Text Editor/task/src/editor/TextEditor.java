5af94a0a99aa25270e641fa0a4ace8dd4745d5a3
bastihz <bastihz.dev@posteo.de>
2021-01-19 20:50:20 +0100

    Stage 3 done

--- Text_Editor/Text Editor/task/src/editor/TextEditor.java
+++ Text_Editor/Text Editor/task/src/editor/TextEditor.java
@@ -2,6 +2,8 @@ package editor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,6 +11,26 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class TextEditor extends JFrame {
+    private JTextField filenameField;
+    private JTextArea textArea;
+    private final ActionListener saveFileListener = actionEvent -> {
+        File file = new File(filenameField.getText());
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(textArea.getText());
+        } catch (IOException e) {
+            System.out.printf("An exception occurred while saving %s\n", e.getMessage());
+        }
+    };
+    private final ActionListener loadFileListener = actionEvent -> {
+        String text = "";
+        try {
+            text = new String(Files.readAllBytes(Paths.get(filenameField.getText())));
+        } catch (IOException e) {
+            System.out.printf("Cannot read file %s\n", e.getMessage());
+        }
+        textArea.setText(text);
+    };
+
 
     public TextEditor() {
         super("Text Editor");
@@ -16,48 +38,71 @@ public class TextEditor extends JFrame {
         setSize(400, 500);  // this includes the title bar at the top
         setLocationRelativeTo(null);
 
-        JPanel controlPanel = new JPanel();
-        controlPanel.setLayout(new FlowLayout());
-        add(controlPanel, BorderLayout.NORTH);
-
-        JTextField filenameField = new JTextField();
-        filenameField.setName("FilenameField");
-        controlPanel.add(filenameField);
-
-        JTextArea textArea = new JTextArea();
-        textArea.setName("TextArea");
-
-        JButton saveButton = new JButton("Save");
-        saveButton.setName("SaveButton");
-        saveButton.addActionListener(actionEvent -> {
-            File file = new File(filenameField.getText());
-            try (FileWriter writer = new FileWriter(file)) {
-                writer.write(textArea.getText());
-            } catch (IOException e) {
-                System.out.printf("An exception occurred while saving %s", e.getMessage());
-            }
-        });
-        controlPanel.add(saveButton);
-
-        filenameField.setPreferredSize(new Dimension(150, saveButton.getPreferredSize().height));
-
-        JButton loadButton = new JButton("Load");
-        loadButton.setName("LoadButton");
-        loadButton.addActionListener(actionEvent -> {
-            String text = "";
-            try {
-                text = new String(Files.readAllBytes(Paths.get(filenameField.getText())));
-            } catch (IOException e) {
-                System.out.printf("Cannot read file %s", e.getMessage());
-            }
-            textArea.setText(text);
-        });
-        controlPanel.add(loadButton);
-
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setName("ScrollPane");
-        add(scrollPane);
+        initMenuBar();
+        initSaveLoadPanel();
+        initTextArea();
 
         setVisible(true);  // has to come last to make everything visible immediately
     }
+
+    private void initMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setName("MenuFile");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+
+        JMenuItem loadMenuItem = new JMenuItem("Load");
+        loadMenuItem.setName("MenuLoad");
+        loadMenuItem.addActionListener(loadFileListener);
+        fileMenu.add(loadMenuItem);
+
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.setName("MenuSave");
+        saveMenuItem.addActionListener(saveFileListener);
+        fileMenu.add(saveMenuItem);
+
+        fileMenu.addSeparator();
+
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.setName("MenuExit");
+        exitMenuItem.addActionListener(actionEvent -> System.exit(0));
+        fileMenu.add(exitMenuItem);
+
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
+    }
+
+    private void initSaveLoadPanel() {
+        JPanel saveLoadPanel = new JPanel();
+        saveLoadPanel.setLayout(new FlowLayout());
+        saveLoadPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
+        add(saveLoadPanel, BorderLayout.NORTH);
+
+        filenameField = new JTextField();
+        filenameField.setName("FilenameField");
+        saveLoadPanel.add(filenameField);
+
+        JButton saveButton = new JButton("Save");
+        saveButton.setName("SaveButton");
+        saveButton.addActionListener(saveFileListener);
+        saveLoadPanel.add(saveButton);
+
+        JButton loadButton = new JButton("Load");
+        loadButton.setName("LoadButton");
+        loadButton.addActionListener(loadFileListener);
+        saveLoadPanel.add(loadButton);
+
+        filenameField.setPreferredSize(new Dimension(150, saveButton.getPreferredSize().height));
+    }
+
+    private void initTextArea() {
+        textArea = new JTextArea();
+        textArea.setName("TextArea");
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setName("ScrollPane");
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 10));
+        add(scrollPane);
+    }
+
 }
