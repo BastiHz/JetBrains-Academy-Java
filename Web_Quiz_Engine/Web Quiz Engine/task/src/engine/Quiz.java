4d601c028fa62f6735851cd6631a497dfcb0a8a1
bastihz <bastihz.dev@posteo.de>
2021-02-24 14:40:14 +0100

    Stage 4 done

--- Web_Quiz_Engine/Web Quiz Engine/task/src/engine/Quiz.java
+++ Web_Quiz_Engine/Web Quiz Engine/task/src/engine/Quiz.java
@@ -3,12 +3,19 @@ package engine;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Quiz {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @NotBlank
     private String title;
@@ -19,32 +26,16 @@ public class Quiz {
     private String[] options;
     private int[] answer = new int[0];
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
+    public long getId() {
         return id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setText(String text) {
-        this.text = text;
-    }
-
     public String getText() {
         return text;
-    }
-
-    public void setOptions(String[] options) {
-        this.options = options;
     }
 
     public String[] getOptions() {
--- Web_Quiz_Engine/Web Quiz Engine/task/src/engine/QuizController.java
+++ Web_Quiz_Engine/Web Quiz Engine/task/src/engine/QuizController.java
@@ -1,12 +1,13 @@
 package engine;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,34 +15,31 @@ import java.util.List;
 @Validated
 public class QuizController {
 
-    private final List<Quiz> quizzes = new ArrayList<>();
-    private static final String ID_NOT_FOUND_MESSAGE = "Quiz not found.";
+    @Autowired
+    QuizRepository quizRepository;
+    private static final ResponseStatusException idNotFoundException =
+        new ResponseStatusException(HttpStatus.NOT_FOUND, "Id not found.");
 
     @PostMapping(path = "/api/quizzes", consumes = "application/json")
     Quiz addQuiz(@RequestBody @Valid Quiz quiz) {
-        quiz.setId(quizzes.size());
-        quizzes.add(quiz);
-        return quiz;
+        return quizRepository.save(quiz);
     }
 
     @GetMapping(path = "/api/quizzes/{id}")
     Quiz getQuiz(@PathVariable @Min(0) int id) {
-        return quizzes.get(id);
+        return quizRepository.findById(id)
+            .orElseThrow(() -> idNotFoundException);
     }
 
     @GetMapping(path = "/api/quizzes")
     List<Quiz> getAllQuizzes() {
-        return quizzes;
+        return quizRepository.findAll();
     }
 
     @PostMapping(path = "/api/quizzes/{id}/solve")
     Response checkAnswer(@PathVariable @Min(0) int id, @RequestBody Answer answer) {
-        Quiz quiz = quizzes.get(id);
+        Quiz quiz = quizRepository.findById(id)
+            .orElseThrow(() -> idNotFoundException);
         return Response.get(Arrays.equals(quiz.getAnswer(), answer.getAnswer()));
-    }
-
-    @ExceptionHandler(IndexOutOfBoundsException.class)
-    @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = ID_NOT_FOUND_MESSAGE)
-    void handleIndexOutOfBoundsException() {
     }
 }
