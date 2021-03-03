603f934ecd8a7632060b6d52311b17a925c64d77
bastihz <bastihz.dev@posteo.de>
2021-03-03 19:02:13 +0100

    Get freemarker to work

--- Code_Sharing_Platform/Code Sharing Platform/task/src/platform/Code.java
+++ Code_Sharing_Platform/Code Sharing Platform/task/src/platform/Code.java
@@ -1,14 +1,23 @@
 package platform;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Code {
+
+    private static final DateTimeFormatter dateTimeFormatter =
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     private String code;
     private LocalDateTime date;
+    private String formattedDate;
 
     public void setCode(String code) {
         this.code = code;
         date = LocalDateTime.now();
+        formattedDate = date.format(dateTimeFormatter);
     }
 
     public String getCode() {
@@ -17,5 +26,10 @@ public class Code {
 
     public LocalDateTime getDate() {
         return date;
+    }
+
+    @JsonIgnore
+    public String getFormattedDate() {
+        return formattedDate;
     }
 }
--- Code_Sharing_Platform/Code Sharing Platform/task/src/platform/WebController.java
+++ Code_Sharing_Platform/Code Sharing Platform/task/src/platform/WebController.java
@@ -2,35 +2,29 @@ package platform;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.time.format.DateTimeFormatter;
-
-@RestController
+@Controller  // needs to be @Controller not @RestController or else templates won't load
 @RequestMapping(produces = MediaType.TEXT_HTML_VALUE)
 public class WebController {
 
     @Autowired
     private CodeSnippetService codeSnippetService;
-    private static final String CODE_HTML = ResourceReader.readFileToString("code.html");
-    private static final String CREATE_HTML = ResourceReader.readFileToString("create.html");
-    private static final DateTimeFormatter dateTimeFormatter =
-        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @GetMapping(path = "/code")
-    public String getCodeHtml() {
+    public ModelAndView getCodeHtml(ModelAndView mav) {
         Code code = codeSnippetService.getCodeSnippet();
-        return String.format(
-            CODE_HTML,
-            code.getDate().format(dateTimeFormatter),
-            code.getCode()
-        );
+        mav.setViewName("code");
+        mav.addObject("date", code.getFormattedDate());
+        mav.addObject("code", code.getCode());
+        return mav;
     }
 
     @GetMapping(path = "/code/new")
     public String getCreateHtml() {
-        return CREATE_HTML;
+        return "code_new";
     }
 }
